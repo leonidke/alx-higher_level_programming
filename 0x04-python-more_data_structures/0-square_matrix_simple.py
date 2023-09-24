60af1d4c1436a4135b665eeee62a7c348172e925
leonidke <leonisawe@gmail.com>
2023-09-24 19:58:32 +0300

    Square elements of matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for element in row:
+            new_row.append(element ** 2)
+        new_matrix.append(new_row)
     return new_matrix
